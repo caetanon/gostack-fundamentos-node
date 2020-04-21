fa559025d5c390c54f2d987ed2432c923262db6f
Caetano Neto <caetano.n@gmail.com>
2020-04-21 08:10:31 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,33 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const listOfIncome = this.transactions.filter(i => i.type === 'income');
+    const income = listOfIncome.reduce((t, c) => t + c.value, 0);
+    const listOfOutcome = this.transactions.filter(o => o.type === 'outcome');
+    const outcome: number = listOfOutcome.reduce((t, c) => t + c.value, 0);
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
